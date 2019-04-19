36cb1178f4040c2e8b46b2e89d2e6b00bde63dcf
Stefan van Groningen <groninge@hotmail.com>
2019-04-19 20:48:33 +0200

    Simplified templates

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react"
-import { graphql } from "gatsby"
-import PostIcons from "../components/post-icons"
-import Layout from "../layouts"
-
-import { rhythm } from "../utils/typography"
 
 class PageTemplate extends Component {
   render() {
     const currentPage = this.props.data.wordpressPage
 
+    console.log(currentPage)
+
     return (
-      <Layout>
+      <div>
         <h1 dangerouslySetInnerHTML={{ __html: currentPage.title }} />
-        <PostIcons node={currentPage} css={{ marginBottom: rhythm(1 / 2) }} />
         <div dangerouslySetInnerHTML={{ __html: currentPage.content }} />
-      </Layout>
+
+        <p dangerouslySetInnerHTML={{ __html: currentPage.date }} />
+        <p dangerouslySetInnerHTML={{ __html: currentPage.slug }} />
+      </div>
     )
   }
 }
@@ -22,17 +21,18 @@ class PageTemplate extends Component {
 export default PageTemplate
 
 export const pageQuery = graphql`
-  query($id: String!) {
+  query currentPageQuery($id: String!) {
     wordpressPage(id: { eq: $id }) {
       title
       content
+      slug
+      id
       date(formatString: "MMMM DD, YYYY")
     }
     site {
       id
       siteMetadata {
         title
-        subtitle
       }
     }
   }
